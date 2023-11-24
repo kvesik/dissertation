aeb4b8bf7c6cbb9eb4059825e50860e8b96dbea5
kvesik <33950744+kvesik@users.noreply.github.com>
2023-11-23 21:29:16 -0800

    updated R trajectories script and pythonoutputs-to-R util; renamed some directories

---
+++ scripts/dialect_learninghistory_utils.py
@@ -0,0 +1,46 @@
+import io
+import os
+import pandas as pd
+import re
+import statistics
+from itertools import product
+
+HISTORYFILES = [
+    # "K407p - OT_Soft_KE_iFBfb_forGLA_cat_starBQP2cons_no2xcounts_HISTORY50000.txt",
+    "simulation_outputs/20231107_Magri_outputs/Fi153_python_OTSoft-PDDP-Fin_GLA/Fi153 - OTSoft-PDDP-Fin_GLA_HISTORY1000.txt",
+    "simulation_outputs/20231107_Magri_outputs/NE153_python_OTSoft-PDDP-NEst_GLA/NE153 - OTSoft-PDDP-NEst_GLA_HISTORY1000.txt"
+]
+
+
+def convert_historyfile(sourcepath, destpath):
+    replacementpairs = {
+        "õ":"7",
+        "trial num":"trialnum",
+        "*":"star_",
+        "(":"",
+        ")":""
+    }
+    with io.open(sourcepath, "r") as src:
+        with io.open(destpath, "w", encoding='utf-8') as dest:
+
+            sline = src.readline()
+
+            while len(sline) != 0:
+                if "TOTAL" not in sline:
+                    dline = sline
+                    for target in replacementpairs.keys():
+                        dline = dline.replace(target, replacementpairs[target])
+                    dest.write(dline)
+                sline = src.readline()
+
+
+def main():
+    for hfile in HISTORYFILES:
+        hpath = os.path.join("..", hfile)
+        outputfile = hpath.replace(" - ", "_").replace(".txt", "_forR.txt")
+
+        convert_historyfile(hpath, outputfile)
+
+
+if __name__ == "__main__":
+    main()

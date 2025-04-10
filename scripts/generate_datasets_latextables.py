d5584ace1556c7408b5fcedca21eaf8b0e85e706
kvesik <33950744+kvesik@users.noreply.github.com>
2025-04-09 22:09:27 -0700

    for generating datasets tables in latex

--- scripts/generate_datasets_latextables.py
+++ scripts/generate_datasets_latextables.py
@@ -1,19 +1,102 @@
+import io
+
+from generate_tableaux_otsoft_specifylang import makeinputstrings, iswordinlang, NEst, Fin, NSeto, i, e, A, y, O, I, E, a, u, o, front, back
+
+vowels = {
+    front: [i, e, A, y, O],
+    back: [I, E, a, u, o]
+}
+allsegments = vowels[front] + vowels[back]
+yes = "y"
+no = "n"
+
+latexlookup = {
+    i: i,
+    e: e,
+    A: "\\ae",
+    y: y,
+    O: "\\o",
+    I: "\\textturnm",
+    E: "\\textramshorns",
+    a: "\\textscripta",
+    u: u,
+    o: o,
+    ".": "."
+}
 
 
-from generate_tableaux_otsoft_specifylang import makeinputstrings, allsegments, iswordinlang, NEst, Fin, NSeto
+# modifies the input dict in-place
+def padlists(seqlenlists):
+    # {2: {k: [] for k in vowels[fb]}, 3: {k: [] for k in vowels[fb]}}
+    toreturn = []
+    for seqlen, wordlists in seqlenlists.items():
+        maxlen = max([len(wdlist) for wdlist in wordlists.values()])
+        toreturn.append(maxlen)
+        for k, v in wordlists.items():
+            if len(v) < maxlen:
+                seqlenlists[seqlen][k].extend([""]*(maxlen - len(v)))
+    return tuple(toreturn)
 
-length2words = makeinputstrings([allsegments, allsegments], ucla=True)
-# length2words_nodelims = [wd.replace(".", "") for wd in length2words]
-# length2words_3dotdelims = [wd.replace(".", "...") for wd in length2words]
-length2words_pairs = [(wd.replace(".", ""), wd.replace(".", "...")) for wd in length2words]
 
-length3words = makeinputstrings([allsegments, allsegments, allsegments], ucla=True)
-# length3words_nodelims = [wd.replace(".", "") for wd in length3words]
-# length3words_3dotdelims = [wd.replace(".", "...") for wd in length3words]
-length3words_pairs = [(wd.replace(".", ""), wd.replace(".", "...")) for wd in length3words]
+def formatforlatex(vowelseq):
+    return "".join([latexlookup[v] for v in vowelseq])
 
 
+def buildline(wordsdict, idx, withstar=False):
+    linetowrite = ""
+    for k in (vowels[front] if i in wordsdict.keys() else vowels[back]):
+        linetowrite += " & " + wordsdict[k][idx]
+    linetowrite += " \\\\" + ("*" if withstar else "") + " \n"
+    linetowrite = "    " + linetowrite.strip(" & ")
+    return linetowrite
+
+
+def writeliststofile(lang, frontorback, yesorno, lists):
+    # {2: {k: [] for k in vowels[fb]}, 3: {k: [] for k in vowels[fb]}}
+    filename = "{}_{}_data_{}.txt".format(lang, frontorback, yesorno)
+    print("populating " + filename + " ...")
+    maxlen2, maxlen3 = padlists(lists)
+
+    with io.open(filename, "w") as outfile:
+        for idx in range(maxlen2):
+            outfile.write(buildline(lists[2], idx, withstar=True))
+        outfile.write("    \\midrule\n")
+        for idx in range(maxlen3):
+            outfile.write(buildline(lists[3], idx))
+        outfile.write("    \\bottomrule\n")
 
 
 if __name__ == "__main__":
-    print("TODO")
+    length2wordsfront = makeinputstrings([vowels[front], allsegments], ucla=True)
+    length2wordsback = makeinputstrings([vowels[back], allsegments], ucla=True)
+
+    length3wordsfront = makeinputstrings([vowels[front], allsegments, allsegments], ucla=True)
+    length3wordsback = makeinputstrings([vowels[back], allsegments, allsegments], ucla=True)
+
+    wordpairlists = {
+        front: {
+            2: [(wd.replace(".", ""), formatforlatex(wd.replace(".", "..."))) for wd in length2wordsfront],
+            3: [(wd.replace(".", ""), formatforlatex(wd.replace(".", "..."))) for wd in length3wordsfront]
+        },
+        back: {
+            2: [(wd.replace(".", ""), formatforlatex(wd.replace(".", "..."))) for wd in length2wordsback],
+            3: [(wd.replace(".", ""), formatforlatex(wd.replace(".", "..."))) for wd in length3wordsback]
+        }
+    }
+
+    for lang in [NEst, Fin, NSeto]:
+        for fb in [front, back]:
+            yesnolists = {
+                yes: {2: {k: [] for k in vowels[fb]}, 3: {k: [] for k in vowels[fb]}},
+                no: {2: {k: [] for k in vowels[fb]}, 3: {k: [] for k in vowels[fb]}},
+            }
+
+            for seqlen in [2, 3]:
+                for wdpair in wordpairlists[fb][seqlen]:
+                    if iswordinlang(wdpair[0], lang):
+                        yesnolists[yes][seqlen][wdpair[0][0]].append(wdpair[1])
+                    else:
+                        yesnolists[no][seqlen][wdpair[0][0]].append(wdpair[1])
+
+            for yn in [yes, no]:
+                writeliststofile(lang, fb, yn, yesnolists[yn])
